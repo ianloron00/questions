0c39955a0b277b50da1f445ff77453590361c45e
Ian Loron de Almeida <i198933@dac.unicamp.br>
2022-08-04 00:44:52 -0300

    developing js. Not removing question text properly.

--- app.js
+++ app.js
@@ -1,2 +1,22 @@
 //using selectors inside the element
 // traversing the dom
+
+"use strict";
+// const questions = document.getElementsByClassName("question");
+const questions = document.querySelectorAll(".question");
+
+questions.forEach( (question) => {
+        const btn = question.querySelector(".question-btn");
+        
+        btn.addEventListener("click", () => {
+           
+            questions.forEach( (item) => {
+                if ( item !== question ) {
+                    item.classList.remove( "question-text" );
+                }
+            });
+
+            question.classList.toggle( "question-text" );
+        });
+    }
+);
